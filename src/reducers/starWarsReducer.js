38dae3f7b2e271c34e87a80ca0747df9695ccbc6
jackcornellober <jackcornellober@gmail.com>
2019-06-19 16:13:09 -0700

    everything is just about done but theres a bug I cant figure out

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,14 +1,40 @@
-import /* we need our action types here*/ "../actions";
+import {FETCH_DATA_START} from "../actions";
+import {FETCH_DATA_SUCCESS} from "../actions";
+import {FETCH_DATA_ERROR} from "../actions";
+
+
+
 const initialState = {
-  characters: []
+  characters: [],
+  isFetching: true,
+  error: '',
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_DATA_START:
+      return {
+        ...state,
+        error: '',
+        isFetching: true
+      };
+    case FETCH_DATA_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        characters: action.payload
+      };
+    case FETCH_DATA_ERROR:
+      return {
+        ...state,
+        isFetching: false,
+        error: 'Uh oh... something happened 😵!'
+      };
+    default:
+      return state;
+    
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
-    default:
-      return state;
   }
 };
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
-
+import Loader from 'react-loader-spinner'
 import { CharacterList } from "../components";
+import { getCharacters } from '../actions'
+
 // import actions
 
 class CharacterListView extends React.Component {
@@ -10,26 +12,35 @@ class CharacterListView extends React.Component {
   }
 
   componentDidMount() {
-    // call our action
+    console.log('ListView Mounted')
+    this.props.getCharacters()
   }
 
   render() {
-    if (this.props.fetching) {
+    if (this.props.isFetching) {
       // return something here to indicate that you are fetching data
+      return(
+      <Loader type="Ball-Triangle" color="#00BFFF" height="90" width="60" />
+      )
     }
-    return (
-      <div className="CharactersList_wrapper">
-        <CharacterList characters={this.props.characters} />
-      </div>
-    );
+    // if (this.props.characters.length > 0)
+    // return (
+    //   <div className="CharactersList_wrapper">
+    //     <CharacterList characters={this.props.characters} />
+    //   </div>
+    // );
   }
 }
+
+const mapStateToProps = state => ({
+  error: state.error,
+  isFetching: state.isFetching,
+  characters: state.characters
+});
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
-)(CharacterListView);
+  mapStateToProps, 
+  { getCharacters }
+  )(CharacterListView);
